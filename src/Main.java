e8d8dd6caf06f20e7aa3ec53da7bd2370e9d3bb9
junhyeongleee <djskal3745@gmail.com>
2022-07-05 20:41:34 +0900

    _09019 (Not Completed)

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,13 @@ public class Main {
 
     private static StringTokenizer st;
 
-    private static int N, M, K, result = Integer.MAX_VALUE;
+    private static int T, N, M, K, result = Integer.MAX_VALUE;
+    private static char[] how;
+    private static boolean[] visited;
+    private static int[] from;
 
-    private static int[][] dist;
-
-    private static int[] d1 = new int[]{-2, -2, 0, 0, 2, 2};
-    private static int[] d2 = new int[]{-1, 1, 2, -2, -1, 1};
-    static int c1, c2;
-
-    static Queue<Point> queue;
-
-    static class Point {
-        int p1;
-        int p2;
-
-        public Point(int p1, int p2) {
-            this.p1 = p1;
-            this.p2 = p2;
-        }
-    }
+    private static char[] dslr = new char[]{'D', 'S', 'L', 'R'};
+    private static Queue<Integer> queue;
 
     public static void main(String[] args) throws IOException {
 
@@ -34,47 +22,67 @@ public class Main {
         br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.valueOf(st.nextToken());
+        T = Integer.valueOf(st.nextToken());
 
-        dist = new int[N][N];
+        if(T == 0) return;
 
-        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<T;i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.valueOf(st.nextToken());
+            int b = Integer.valueOf(st.nextToken());
 
-        int r1 = Integer.valueOf(st.nextToken());
-        int r2 = Integer.valueOf(st.nextToken());
-        c1 = Integer.valueOf(st.nextToken());
-        c2 = Integer.valueOf(st.nextToken());
-
-        System.out.print(go(r1, r2));
+            bw.write(bfs(s, b) + "\n");
+        }
 
         bw.flush();
         bw.close();
     }
 
-    public static int go(int p1, int p2) {
-
+    static String bfs(int s, int b){
         queue = new LinkedList<>();
-        queue.add(new Point(p1, p2));
+        how = new char[10000];
+        from = new int[10000];
+        visited = new boolean[10000];
+        queue.add(s);
+        visited[s] = true;
 
-        while (!queue.isEmpty()) {
-            Point p = queue.peek();
-
-            for(int i=0;i<6;i++){
-                int np1 = p.p1 + d1[i];
-                int np2 = p.p2 + d2[i];
-
-                if(np1 >= 0 && np2 >= 0 && np1 < N && np2 < N){
-                    if(np1 == c1 && np2 == c2) return dist[p.p1][p.p2] + 1;
-
-                    if(dist[np1][np2] == 0){
-                        queue.add(new Point(np1, np2));
-                        dist[np1][np2] += dist[p.p1][p.p2] + 1;
-                    }
-                }
+        while(!queue.isEmpty()){
+            int num = queue.remove();
+            int n_num = num*2 % 10000;
+            if(!visited[n_num]){
+                visited[n_num] = true;
+                how[n_num] = 'D';
+                from[n_num] = num;
+                queue.add(n_num);
             }
-            queue.remove();
+            n_num = num == 0 ? 9999 : num - 1;
+            if(!visited[n_num]){
+                visited[n_num] = true;
+                how[n_num] = 'S';
+                from[n_num] = num;
+                queue.add(n_num);
+            }
+            n_num = num % 1000 * 10 + num / 1000;
+            if(!visited[n_num]){
+                visited[n_num] = true;
+                how[n_num] = 'L';
+                from[n_num] = num;
+                queue.add(n_num);
+            }
+            n_num = num%10 * 1000 + num/10;
+            if(!visited[n_num]){
+                visited[n_num] = true;
+                how[n_num] = 'R';
+                from[n_num] = num;
+                queue.add(n_num);
+            }
         }
 
-        return -1;
+        StringBuilder sb = new StringBuilder();
+        while( s != b){
+            sb.append(how[b]);
+            b = from[b];
+        }
+        return sb.toString();
     }
 }
---
+++ src/boj/bfs_dfs/_9019.java
@@ -0,0 +1,61 @@
+package boj.bfs_dfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _9019 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            boolean[] check = new boolean[100010];
+            char[] how = new char[10000];
+            int[] from = new int[10000];
+            check[n] = true;
+            from[n] = -1;
+            Queue<Integer> q = new LinkedList<Integer>();
+            q.add(n);
+            while (!q.isEmpty()) {
+                int now = q.remove();
+                int next = (now*2) % 10000;
+                if (check[next] == false) {
+                    q.add(next);
+                    check[next] = true;
+                    from[next] = now;
+                    how[next] = 'D';
+                }
+                next = now-1;
+                if (next == -1) next = 9999;
+                if (check[next] == false) {
+                    q.add(next);
+                    check[next] = true;
+                    from[next] = now;
+                    how[next] = 'S';
+                }
+                next = (now%1000)*10 + now/1000;
+                if (check[next] == false) {
+                    q.add(next);
+                    check[next] = true;
+                    from[next] = now;
+                    how[next] = 'L';
+                }
+                next = (now/10) + (now%10)*1000;
+                if (check[next] == false) {
+                    q.add(next);
+                    check[next] = true;
+                    from[next] = now;
+                    how[next] = 'R';
+                }
+            }
+            StringBuilder ans = new StringBuilder();
+            while (m != n) {
+                ans.append(how[m]);
+                m = from[m];
+            }
+            System.out.println(ans.reverse());
+        }
+    }
+}
