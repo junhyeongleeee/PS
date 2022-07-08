f58f0044030ac5a770e5c8682fb36c869463947a
junhyeongleee <djskal3745@gmail.com>
2022-07-08 19:58:20 +0900

    _2206 ing...

--- src/Main.java
+++ src/Main.java
@@ -17,26 +17,23 @@ public class Main {
 
     private static BufferedWriter bw;
     private static BufferedReader br;
-
     private static StringTokenizer st;
-
-    private static int N, M, answer = Integer.MIN_VALUE;
+    private static int N, M, A, B, C,sum, answer = Integer.MIN_VALUE;
     private static int[][] area;
-
-    private static int[][] tmp_area;
-
     private static Queue<Point> q;
-
     private static int[] dx = new int[]{0, 0, -1, 1};
     private static int[] dy = new int[]{-1, 1, 0, 0};
 
     private static boolean[][] visited;
+
     private static class Point{
         int x;
         int y;
-        public Point(int x, int y){
+        int crash;
+        public Point(int x, int y, int crash){
             this.x = x;
             this.y = y;
+            this.crash = crash;
         }
     }
     public static void main(String[] args) throws IOException {
@@ -47,77 +44,44 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.valueOf(st.nextToken());
         M = Integer.valueOf(st.nextToken());
+
         area = new int[N][M];
-        tmp_area = new int[N][M];
-        visited = new boolean[N][M];
 
         for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
+            char[] chars = br.readLine().toCharArray();
             for(int j=0;j<M;j++){
-                area[i][j] = Integer.valueOf(st.nextToken());
+                area[i][j] = chars[j] - '0';
             }
         }
-        dfs(0);
-
-        System.out.print(answer);
 
         bw.flush();
         bw.close();
     }
 
-    private static void dfs(int cnt){
-        if(cnt == 3){
-            bfs();
-            return;
-        }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(area[i][j] == 0){
-                    area[i][j] = 1;
-                    dfs(cnt + 1);
-                    area[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    private static void bfs(){
+    static int bfs(int x, int y){
         q = new LinkedList<>();
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                tmp_area[i][j] = area[i][j];
-                if(tmp_area[i][j] == 2)
-                    q.add(new Point(j, i));
-            }
-        }
+        q.add(new Point(0, 0, 0));
 
         while(!q.isEmpty()){
             Point p = q.remove();
+
+            int crash = p.crash;
 
             for(int i=0;i<4;i++){
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
 
                 if(nx >= 0 && ny >= 0 && nx < M && ny < N){
-                    if(tmp_area[ny][nx] == 0){
-                        tmp_area[ny][nx] = 2;
-                        q.add(new Point(nx, ny));
+                    if(area[ny][nx] == 0){
+
+                    }
+                    if(crash == 0 && area[ny][nx] == 1){
+                        q.add(new Point(nx, ny, 1));
                     }
                 }
             }
         }
 
-        int count = 0;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(tmp_area[i][j] == 0)
-                    count++;
-            }
-        }
-
-        answer = Math.max(answer, count);
+        return -1;
     }
-
 }
