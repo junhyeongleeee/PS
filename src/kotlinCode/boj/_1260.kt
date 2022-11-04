0dccb99f3598ce3a7bcde9982feab9a540109180
junhyeongleeee <djskal3745@gmail.com>
2022-11-04 21:02:25 +0900

    _1206

---
+++ src/kotlinCode/boj/_1260.kt
@@ -0,0 +1,61 @@
+package kotlinCode.boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+
+
+val br = System.`in`.bufferedReader()
+val bw = System.out.bufferedWriter()
+
+fun main() = with(br) {
+
+    val (n, m, v) = readLine().split(" ").map { it.toInt() }
+    val array = Array(n + 1) { mutableListOf<Int>() }
+
+    repeat(m) {
+        val (v1, v2) = readLine().split(" ").map { it.toInt() }
+        array[v1].add(v2)
+        array[v2].add(v1)
+    }
+
+    array.forEach {
+        it.sort()
+    }
+
+    var visited = BooleanArray(n + 1)
+    dfs(array, visited, v)
+    visited = BooleanArray(n + 1)
+    bw.write("\n")
+    bfs(array, visited, v)
+
+    bw.flush()
+    bw.close()
+}
+
+fun dfs(array: Array<MutableList<Int>>, visited: BooleanArray, v: Int) {
+    visited[v] = true
+    bw.write("$v ")
+    array[v].forEach {
+        if (!visited[it]) {
+            dfs(array, visited, it)
+        }
+    }
+}
+
+fun bfs(array: Array<MutableList<Int>>, visited: BooleanArray, v: Int) {
+    var queue = LinkedList<Int>()
+    queue.add(v)
+    visited[v] = true
+
+    while (!queue.isEmpty()) {
+        var nv = queue.poll()
+        bw.write("$nv ")
+        array[nv].forEach {
+            if (!visited[it]) {
+                queue.add(it)
+                visited[it] = true
+            }
+        }
+    }
+}
