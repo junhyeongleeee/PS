a6eebac7ef3503a0e4b4e99abf7d7475b4221ecc
junhyeongleeee <djskal3745@gmail.com>
2023-02-10 13:18:33 +0900

    _3584 수정

--- src/kotlinCode/boj/_3584.kt
+++ src/kotlinCode/boj/_3584.kt
@@ -9,61 +9,77 @@ import java.util.StringTokenizer
  *  - 정점 V ( 1<=V <= N)
  *
  *  - LCA(Lowest Common Ancestor) 최소 공통 조상을 찾는 알고리즘
- *
  */
 
 private val br = System.`in`.bufferedReader()
 private lateinit var st: StringTokenizer
 private lateinit var arr: Array<MutableList<Int>>
 private val sb = StringBuilder()
-private var cnt = 0
-private var depth1 = -1
-private var depth2 = -1
+private lateinit var parent: IntArray
+private lateinit var depth: IntArray
+private lateinit var rootCheck: BooleanArray
+
 fun main() = with(br) {
     val t = readLine().toInt()
     repeat(t) {
         val n = readLine().toInt()
         arr = Array(n + 1) { mutableListOf() }
-
-        repeat(n - 1) { i ->
+        parent = IntArray(n + 1)
+        depth = IntArray(n + 1)
+        rootCheck = BooleanArray(n + 1)
+        repeat(n - 1) {
             st = StringTokenizer(readLine())
             val v1 = st.nextToken().toInt()
             val v2 = st.nextToken().toInt()
             arr[v1].add(v2)
+            rootCheck[v2] = true
+            parent[v2] = v1
         }
+        val root = findRoot()
+        init(root, 0)
 
         st = StringTokenizer(readLine())
         val v1 = st.nextToken().toInt()
         val v2 = st.nextToken().toInt()
 
-        var minDepth = Integer.MAX_VALUE
-        var result = 0
-
-        for (i in 1..n) {
-            cnt = 0
-            dfs3584(i, 0, v1, v2)
-            val depth = depth1 + depth2
-            if (minDepth > depth && cnt == 2) {
-                minDepth = depth
-                result = i
-            }
-        }
-        sb.append(result).append("\n")
+        sb.append(LCA(v1, v2)).append("\n")
     }
     println(sb)
 }
 
-fun dfs3584(s: Int, depth: Int, v1: Int, v2: Int) {
+fun findRoot(): Int {
+    for (i in 1 until rootCheck.size) {
+        if (!rootCheck[i]) {
+            return i
+        }
+    }
+    return -1
+}
 
-    if (s == v1) {
-        cnt++
-        depth1 = depth
-    }
-    if (s == v2) {
-        cnt++
-        depth2 = depth
-    }
-    arr[s].forEach {
-        dfs3584(it, depth + 1, v1, v2)
+fun init(r: Int, d: Int) {
+    depth[r] = d
+    arr[r].forEach {
+        init(it, d + 1)
     }
 }
+
+fun LCA(a: Int, b: Int): Int {
+    var ah = depth[a]
+    var bh = depth[b]
+    var tA = a
+    var tB = b
+    while (ah > bh) {
+        tA = parent[tA]
+        ah--
+    }
+    while (bh > ah) {
+        tB = parent[tB]
+        bh--
+    }
+    while (tA != tB) {
+        tA = parent[tA]
+        tB = parent[tB]
+    }
+    return tA
+}
+
