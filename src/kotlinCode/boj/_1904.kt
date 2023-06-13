2121192fb61078c9635741781a390fc9d5db6d4f
junhyeongleeee <djskal3745@gmail.com>
2023-06-13 19:27:45 +0900

    1904 01타일

---
+++ src/kotlinCode/boj/_1904.kt
@@ -0,0 +1,20 @@
+package kotlinCode.boj
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+
+    val dp = IntArray(n + 1)
+
+    if (n >= 1) {
+        dp[1] = 1
+    }
+    if (n >= 2) {
+        dp[2] = 2
+    }
+
+    for (i in 3 .. n) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+    }
+
+    println(dp[n])
+}
