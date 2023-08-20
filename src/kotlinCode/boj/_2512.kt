74d20301ad15d8d17ec75a19ea7b60b293c8b763
junhyeongleeee <djskal3745@gmail.com>
2023-08-20 14:37:07 +0900

    2512 예산 (이분탐색)

--- src/kotlinCode/boj/_2512.kt
+++ src/kotlinCode/boj/_2512.kt
@@ -1,41 +1,41 @@
 package kotlinCode.boj
 
-import java.util.StringTokenizer
-import kotlin.math.max
+import java.util.*
 
 private lateinit var st: StringTokenizer
 private lateinit var arr: IntArray
-private var answer = Int.MIN_VALUE
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()          // 3 <=  <= 10,000
 
     arr = IntArray(n)
 
     st = StringTokenizer(readLine())
+
     repeat(n) {
-        val v = st.nextToken().toInt()  // 1 <=  <= 100,000
-        arr[it] = v
+        arr[it] = st.nextToken().toInt()  // 1 <=  <= 100,000
     }
     val m = readLine().toInt()          // n <=  <= 1,000,000,000
 
-    arr.sortDescending()
+    var left = 0
+    var right = arr.maxOf { it }
 
-    val diff = arr.sum() - m
-    if (diff <= 0) {
-        println(arr[0])
-    } else {
-        for (i in 0 until n) {
+    if (arr.sum() <= m) {
+        println(right)
+    }
+    else {
+        // lowBound
+        while (left + 1< right) {
+            val mid = (left + right) / 2
             var sum = 0
-            val min = arr[i]
-            for (j in 0 .. i) {
-                sum += arr[j]
+            for (i in 0 until n) {
+                sum += if (arr[i] < mid) arr[i] else mid
             }
-            val result = (sum - diff) / (i + 1)
-            if (min < result) {
-                break
+            if (m >= sum) {
+                left = mid
+            }else {
+                right = mid
             }
-            answer = max(answer, result)
         }
-        println(answer)
+        println(left)
     }
 }
